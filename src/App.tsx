83674d92296ac9dd414c6b9fc851de95168e8cfd
estevao242 <estevao.santos@agencia242.com.br>
2023-04-27 08:30:09 -0300

    Ex formulário simples react b7 web

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
+
 const App = () => {
+ const  [firstName, setName] = useState('Usuário');
+ const  [lastName, setLastName] = useState('');
+ const  [age, setAge]  = useState('');
+
+ const handleInputName = (event: React.ChangeEvent<HTMLInputElement>) => {
+  event.target.value = event.target.value.replace(/[^\p{L}\s]/gu, '')
+  setName(event.target.value)
+ }
+ const handleInputLastName = (event: React.ChangeEvent<HTMLInputElement>) => {
+  event.target.value = event.target.value.replace(/[^\p{L}\s]/gu, '')
+  setLastName(event.target.value)
+ }
+ const handleInputAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+  event.target.value = event.target.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1')
+  setAge(event.target.value)
+ }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={firstName} onChange={handleInputName} />
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={lastName} onChange={handleInputLastName} />
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={age} onChange={handleInputAge} />
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {firstName} {lastName}, tudo bem?<br/>
+      Você tem {age} anos.
     </div>
   );
 }
